4eb843ae4f86f1903f533719835a32ea7875717a
Crox-x <saradgaihre19@gmail.com>
2022-01-10 17:48:40 +0000

    works ui wise(nearly)

--- frontend/croxx_fileshare/src/components/Form.jsx
+++ frontend/croxx_fileshare/src/components/Form.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import './form.css';
-
-export default function Form(){
-    async function uploadHandel(){
-        
-        let [fileHandle] = await window.showOpenFilePicker();
-        const file = await fileHandle.getFile();
-        const fileVlaue = await file.stream();
-        console.log(fileVlaue);
+export default class Form extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            fileName: ""
+        };
+    }
+    fileSelectHandel= async ()=>{
+            let [fileHandle] = await window.showOpenFilePicker();
+            const file = await fileHandle.getFile();
+            const fileName = await file.name;
+            const fileVlaue = await file.stream();
+            this.setState({fileName: fileName});
+            console.log(fileVlaue);
 
     };
+
+
+    render(){    
         return(
             <div className='parentContainerForm'>
                     <div className='uploadButton'>
-                        <button id='fileUpload' className='input-button' type="file" name='file' onClick={uploadHandel}>
+                        <button id='fileUpload' className='input-button' type="file" name='file' onClick={this.fileSelectHandel}>
                             Select Files
                             </button>
                     </div>
+
                     <div className='accessUrl'>
                     <p>Acess Url:</p>
                     <input className='titleEnter' type='text'/>
                     </div>
-                    
+                    <div id='fileName'>
+                        <p><u>{this.state.fileName}</u></p>
+                    </div>
                     <button className='submit-button' type="submit">Upload</button>
             </div>
         );
 
+}
 }
